32f91d5ee8eb2a3432d1caa21b5459acb9d9ade1
ernesto-ferreyra <ernesto.ferreyra@globant.com>
2022-02-01 12:34:50 -0300

    [Feature] - creamos archivos para ver el header

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Categories from '../screens/categories/index';
 import Products from '../screens/products/index';
 import ProductDetail from '../screens/product-detail/index';
+// import { headerStyle },
+import { COLORS } from '../utils/data/constants/colors';
 
 
 const Stack = createNativeStackNavigator();
@@ -12,7 +14,18 @@ const Stack = createNativeStackNavigator();
 const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Categories'>
+      <Stack.Navigator 
+        initialRouteName='Categories'
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? COLORS.primary : '',
+          },
+          headerTintColor: Platform.OS === 'android' ? 'white' : COLORS.primary,
+          headerTitleStyle: {
+            fontweight: 'bold', 
+          },
+        }}
+      >
         <Stack.Screen name="Categories" component={Categories} />  
         <Stack.Screen name="Products" component={Products} />
         <Stack.Screen name="ProductDetail" component={ProductDetail} />
